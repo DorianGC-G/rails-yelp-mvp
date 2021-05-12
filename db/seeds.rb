43164e1ba21c567fe5313446b8081e056140d1be
Dorian Guitel <dorian.guitel@gmail.com>
2021-05-12 16:50:45 +0200

    finished challenge 2 yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants = Restaurant.create!([
+  {name: 'Au Pied de Cochon', phone_number: '01 40 13 77 00', category: 'French', address: '6 Rue Coquillière, 75001 Paris'},
+  {name: 'Les Pinces', phone_number: '09 83 56 47 93', category: 'French', address: '29 Rue du Bourg Tibourg, 75004 Paris'},
+  {name: 'Bouillon Pigalle', phone_number: '01 42 59 69 31', category: 'French', address: '22 Boulevard de Clichy, 75018 Paris'},
+  {name: 'Higuma', phone_number: '01 47 03 38 59', category: 'Japanese', address: '32bis Rue Sainte-Anne, 75001 Paris'},
+  {name: 'Pizzeria Popolare', phone_number: '01 42 21 30 91', category: 'Italian', address: '111 Rue Réaumur, 75002 Paris'}
+])
